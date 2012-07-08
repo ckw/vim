81bd761663130174294a8e3a969b0cbc6d9776cc
Connor Williams <connor@janrain.com>
2012-07-08 05:43:20 -0700

    create after folder in .vim

--- setupVimPlugins.sh
+++ setupVimPlugins.sh
@@ -2,6 +2,7 @@
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -so ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+cp -r after ~/.vim
 
 cd ~/.vim/bundle
 
