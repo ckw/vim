6cff346bcb850652cd43c60e9ba70414ffd6df49
Connor Williams <connor@janrain.com>
2012-08-12 11:24:23 -0700

    add easy-motion to setup

--- setupVimPlugins.sh
+++ setupVimPlugins.sh
@@ -8,6 +8,10 @@ cd ~/.vim/bundle
 
 git clone git://github.com/tpope/vim-fugitive.git
 git clone https://github.com/vim-scripts/LustyJuggler.git
+echo "remember to change default LustyJuggler invoker to <Leader><Leader>"
+echo "note: you actually have to change the script"
+echo ""
+
 git clone https://github.com/scrooloose/syntastic.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/Lokaltog/vim-powerline.git
@@ -19,6 +23,8 @@ git clone https://github.com/Lokaltog/vim-easymotion/
 
 git clone https://github.com/flazz/vim-colorschemes.git
 ln -s ~/.vim/bundle/colorschemes/colors ~/.vim/colors
+
+git clone https://github.com/Lokaltog/vim-easymotion.git
 
 git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ~/.fonts/ubuntu-mono-powerline-ttf
 fc-cache -vf
