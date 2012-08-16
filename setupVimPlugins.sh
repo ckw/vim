15e01efef04eb69a1e86e2b96f8cf8bf3e835e82
Connor Williams <connor@janrain.com>
2012-08-16 01:35:08 -0700

    add Gundo in setup

--- setupVimPlugins.sh
+++ setupVimPlugins.sh
@@ -14,6 +14,7 @@ git clone git://github.com/tpope/vim-fugitive.git
 git clone https://github.com/vim-scripts/LustyJuggler.git
 git clone git@github.com:ckw/vim-powerline.git
 git clone https://github.com/Lokaltog/vim-easymotion.git
+git clone https://github.com/sjl/gundo.vim.git
 
 git clone https://github.com/flazz/vim-colorschemes.git ~/.vim/vim-colorschemes
 ln -s ~/.vim/vim-colorschemes/colors ~/.vim/colors
